a941ab2c93c5674f7c3955355925f1e96d9c7b4c
teracy164 <literacy1945@gmail.com>
2023-05-28 20:38:34 +0900

    fix url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: process.env.BASE_URL,
+    // baseURL: process.env.BASE_URL,
     cdnURL: process.env.BASE_URL,
   },
   runtimeConfig: {
