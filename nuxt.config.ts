2f61679d99003874006f4943e23be317c61d44cc
teracy164 <literacy1945@gmail.com>
2023-05-28 20:23:37 +0900

    fix .env.template

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: process.env.BASE_URL,
+  },
   runtimeConfig: {
     public: {
       openApiKey: process.env.OPENAI_API_KEY,
